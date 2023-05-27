c9a08c04ed7cd17cfbd80dc2db97202ba34cdcb9
Gabrielle Palar <gabrielle.a.palar@gmail.com>
2023-05-27 16:32:54 -0700

    added skeleton for results page

--- nextjs-blog/tailwind.config.js
+++ nextjs-blog/tailwind.config.js
@@ -1,12 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
+    "./node_modules/flowbite-react/**/*.js",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
 
     // Or if using `src` directory:
     "./src/**/*.{js,ts,jsx,tsx,mdx}",
+    "./node_modules/flowbite/**/*.js"
   ],
   mode: 'jit',
   theme: {
@@ -25,6 +27,6 @@ module.exports = {
     },
   },
   variants: {},
-  plugins: [],
+  plugins: [require('flowbite/plugin')],
 }
 
