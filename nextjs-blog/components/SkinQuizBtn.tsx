13f834ab290d1689df8801523d7ab7825a7a7923
Annesa Tran <annesat2020@gmail.com>
2023-05-28 01:50:26 -0700

    Added SkinQuizBtn component to skinQuiz page

---
+++ nextjs-blog/components/SkinQuizBtn.tsx
@@ -0,0 +1,20 @@
+import styles from '../styles/Home.module.css';
+
+interface skinbuttontype {
+    text: string,
+    color: string,
+    value: boolean,
+    setState: any,
+    icon?: any
+}
+
+function SkinQuizBtn(props:skinbuttontype) {
+    return (
+        <button onClick={() => props.setState(!props.value)} className="w-40 h-40 shadow-lg p-8 rounded-xl bg-dark-purple hover:bg-gray-400 text-black font-bold ">
+            {props.icon}
+            {props.text}
+        </button>
+    )
+}
+
+export default SkinQuizBtn;
