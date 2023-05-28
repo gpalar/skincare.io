5b478ff13e907071085cbe7d9a0ee0ff2f1f60f9
Annesa Tran <annesat2020@gmail.com>
2023-05-28 08:18:26 -0700

    Favicon and tab title

--- nextjs-blog/pages/_app.tsx
+++ nextjs-blog/pages/_app.tsx
@@ -5,6 +5,8 @@ import Head from 'next/head';
 export default function App({ Component, pageProps }) {
     return <>
         <Head>
+            <title>skincare.io</title>
+            <link rel="icon" href="sparkle-logo.png" />
             <link rel="preconnect" href="https://fonts.googleapis.com" />
             <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin='anonymous' />
             <link href="https://fonts.googleapis.com/css2?family=DM+Sans:ital,wght@0,400;0,500;0,700;1,400;1,500;1,700&display=swap" rel="stylesheet" />
