11d26f6c908fb8dde1812ba3d04fd6d1d615e69b
Gabrielle Palar <gabrielle.a.palar@gmail.com>
2023-05-28 02:27:18 -0700

    removed outline from next button

--- nextjs-blog/pages/skinQuiz.tsx
+++ nextjs-blog/pages/skinQuiz.tsx
@@ -180,7 +180,7 @@ export default function skinQuiz() {
                     </div>
                 </div>
 
-                <button className="bg-white hover:bg-gray-400 text-gray-800 font-bold py-2 px-4 rounded inline-flex items-center">
+                <button className="hover:bg-gray-400 text-gray-800 font-bold py-2 px-4 rounded inline-flex items-center">
                     <svg className="fill-current w-6 h-6 mr-2" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor">
                         <path fill-rule="evenodd" d="M12 2.25c-5.385 0-9.75 4.365-9.75 9.75s4.365 9.75 9.75 9.75 9.75-4.365 9.75-9.75S17.385 2.25 12 2.25zm4.28 10.28a.75.75 0 000-1.06l-3-3a.75.75 0 10-1.06 1.06l1.72 1.72H8.25a.75.75 0 000 1.5h5.69l-1.72 1.72a.75.75 0 101.06 1.06l3-3z" clip-rule="evenodd" />
                     </svg>
